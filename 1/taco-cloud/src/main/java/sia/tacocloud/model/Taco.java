93b7b6a0d399fdc35d161d05831780c961139818
murtalaliyu <murtalaliyu10@gmail.com>
2022-11-04 01:02:34 -0400

    Add validation

--- 1/taco-cloud/src/main/java/sia/tacocloud/model/Taco.java
+++ 1/taco-cloud/src/main/java/sia/tacocloud/model/Taco.java
@@ -2,11 +2,18 @@ package sia.tacocloud.model;
 
 import java.util.List;
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
 
+  @NotNull
+  @Size(min=5, message="Name must be at least 5 characters long")
   private String name;
+
+  @NotNull
+  @Size(min=1, message="You must choose at least 1 ingredient")
   private List<Ingredient> ingredients;
   
 }
