7bfd279a8e512fb59ad6de9898ee29c1b82a30f6
murtalaliyu <murtalaliyu10@gmail.com>
2022-11-04 01:01:28 -0400

    Add validation and use logger

--- 1/taco-cloud/src/main/java/sia/tacocloud/controller/OrderController.java
+++ 1/taco-cloud/src/main/java/sia/tacocloud/controller/OrderController.java
@@ -1,6 +1,9 @@
 package sia.tacocloud.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,21 @@ public class OrderController {
   
   @GetMapping("/current")
   public String orderForm() {
-    System.out.println("in orderForm...");
+    log.info("showing orderForm page...");
     return "/orderForm";
   }
 
   @PostMapping
-  public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+  public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+    if (errors.hasErrors()) {
+      log.info(":( errors exist in tacoOrder: {}", order);
+      log.info("the errors are: {}", errors);
+      return "orderForm";
+    }
+    
     log.info("Order submitted: {}", order);
+    sessionStatus.setComplete();
+
     return "redirect:/";
   }
 
