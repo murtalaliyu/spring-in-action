d54958e016622b5938b5e9008cb65c6b17012d32
murtalaliyu <murtalaliyu10@gmail.com>
2022-11-27 19:37:49 -0500

    Lookin good

---
+++ chp7/tacocloud-api/src/main/java/tacos/web/api/TacoController.java
@@ -0,0 +1,37 @@
+package tacos.web.api;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/api/tacos", produces = "application/json")
+@CrossOrigin(origins = "http://tacocloud:8080")
+public class TacoController {
+
+  private TacoRepository tacoRepository;
+
+  public TacoController(TacoRepository tacoRepository) {
+    this.tacoRepository = tacoRepository;
+  }
+
+  @GetMapping(params = "recent")
+  public Iterable<Taco> recentTacos() {
+    PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+    return tacoRepository.findAll(page).getContent();
+  }
+  
+}
---
+++ chp7/tacocloud-data/src/main/java/tacos/data/UserRepository.java
@@ -0,0 +1,11 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+  User findByUsername(String username);
+
+}
