90f61b60d923265b29c578447b9e2e9ddb689f7f
murtalaliyu <murtalaliyu10@gmail.com>
2022-11-29 00:13:08 -0500

    Fully functional as of chp7.1.4

--- chp7/tacocloud-api/src/main/java/tacos/web/api/TacoController.java
+++ chp7/tacocloud-api/src/main/java/tacos/web/api/TacoController.java
@@ -2,17 +2,12 @@ package tacos.web.api;
 
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import tacos.Taco;
 import tacos.data.TacoRepository;
@@ -32,6 +27,41 @@ public class TacoController {
   public Iterable<Taco> recentTacos() {
     PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
     return tacoRepository.findAll(page).getContent();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+    Optional<Taco> optTaco = tacoRepository.findById(id);
+    if (optTaco.isPresent()) return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+
+  @PostMapping(consumes = "application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Taco postTaco(@RequestBody Taco taco) {
+    return tacoRepository.save(taco);
+  }
+
+  @PutMapping(path = "/{tacoId}", consumes = "application/json")
+  public Taco putTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco taco) {
+    taco.setId(tacoId);
+    return tacoRepository.save(taco);
+  }
+
+  @PatchMapping(path = "/{tacoId}", consumes = "application/json")
+  public Taco patchTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco patch) {
+    Taco taco = tacoRepository.findById(tacoId).get();
+    if (patch.getName() != null) taco.setName(patch.getName());
+    if (patch.getIngredients() != null) taco.setIngredients(patch.getIngredients());
+    return tacoRepository.save(taco);
+  }
+
+  @DeleteMapping(path = "/{tacoId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteTaco(@PathVariable("tacoId") Long tacoId) {
+    try {
+      tacoRepository.deleteById(tacoId);
+    } catch (EmptyResultDataAccessException e) {}
   }
   
 }
