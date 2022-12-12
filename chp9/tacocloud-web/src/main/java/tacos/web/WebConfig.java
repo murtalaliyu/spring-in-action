ed63887074464907ea688956e1e4944582db78b3
murtalaliyu <murtalaliyu10@gmail.com>
2022-12-11 23:18:00 -0500

    Finish up tacocloud-web

---
+++ chp9/tacocloud-web/src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,23 @@
+package tacos.web;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+}
