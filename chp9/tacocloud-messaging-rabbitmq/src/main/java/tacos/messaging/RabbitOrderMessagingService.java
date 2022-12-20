ebbf18ae8536d8c1cefc7ed34d9408b801a09289
murtalaliyu <murtalaliyu10@gmail.com>
2022-12-20 00:00:57 -0500

    tacocloud-messaging-rabbitmq is WIP

---
+++ chp9/tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
@@ -0,0 +1,28 @@
+package tacos.messaging;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tacos.TacoOrder;
+
+@Service
+public class RabbitOrderMessagingService implements OrderMessagingService {
+
+    private RabbitTemplate rabbit;
+
+    @Autowired
+    public RabbitOrderMessagingService(RabbitTemplate rabbit) {
+        this.rabbit = rabbit;
+    }
+
+    @Override
+    public void sendOrder(TacoOrder order) {
+        MessageConverter converter = rabbit.getMessageConverter();
+        MessageProperties props = new MessageProperties();
+        Message message = converter.toMessage(order, props);
+        rabbit.send("tacocloud.order", message);
+    }
+}
