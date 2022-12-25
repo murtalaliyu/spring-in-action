bc2d0080d9dba739464dd319fdfdceadc305c63f
murtalaliyu <murtalaliyu10@gmail.com>
2022-12-25 00:14:16 -0500

    chp10 is WIP

---
+++ chp10/simple-flow/src/main/java/sia6/FileWriterGateway.java
@@ -0,0 +1,12 @@
+package sia6;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriterGateway {
+
+    void writeToFile(@Header(FileHeaders.FILENAME) String filename, String data);
+
+}
