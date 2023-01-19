7bbf2b26413ce6c70c99d256137ca704dbb5e5d2
murtalaliyu <murtalaliyu10@gmail.com>
2023-01-19 00:15:11 -0500

    Add IngredientRepository test

---
+++ chp13/tacocloud-r2dbc/tacocloud-data-r2dbc/src/test/java/tacos/data/TestConfig.java
@@ -0,0 +1,9 @@
+package tacos.data;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class TestConfig {
+}
